be99dd64af6d221053957453397679aaa3b01584
Andrés Pineda <andres.pineda.8991@gmail.com>
2026-04-28 23:14:02 -0500

    Files for: Token demo

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# Lunch_Learn_vol1_start_using_LLMs
-In this respository, you can find demos to start using LLMs from zero
+# LLM Chatbot Console Tool
+
+Script en Python para interactuar con modelos de lenguaje (LLMs) desde consola, permitiendo estimar costes y personalizar respuestas.
+
+---
+
+## Descripción
+
+El programa permite:
+
+- Introducir preguntas por consola  
+- Estimar tokens de entrada  
+- Comparar costes entre modelos  
+- Seleccionar modelo  
+- Configurar límite de tokens  
+- Ajustar creatividad (temperature)  
+- Obtener respuesta del modelo  
+
+---
+
+## Técnica
+
+- Estimación: **1 token ≈ 4 caracteres**
+- Coste: calculado según precio por **1M tokens**
+- Parámetros:
+  - `max_tokens`: longitud de respuesta  
+  - `temperature`: nivel de creatividad  
+
+---
+
+## Arquitectura
+main()
+├── estimate_tokens()
+├── estimate_cost()
+├── select_model()
+├── select_max_tokens()
+├── select_temperature()
+└── query_model()
+
+# Configuración
+
+Agregar la key en el achivo .env:
+
+# Modelos
+Modelo de precio de input según la página de openAI (https://platform.openai.com/pricing):
+gpt-4o-mini	$0.15 / 1M
+gpt-4.1-mini	$0.40 / 1M
+gpt-4.1	$2.00 / 1M
+gpt-5-nano	$0.05 / 1M
+gpt-5-mini	$0.25 / 1M
---
+++ Slide_tokens.py
@@ -0,0 +1,269 @@
+import math
+import os
+import time
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+# Obtener API key
+# Leer el archivo README.md para más información de la API
+api_key = os.getenv("OPENAI_API_KEY")
+assert api_key is not None, "Define OPENAI_API_KEY en el .env"
+
+# Crear cliente
+client = OpenAI(api_key=api_key)
+
+print("API key cargada correctamente")
+
+
+# -----------------------------
+# CONFIGURACIÓN DE MODELOS
+# -----------------------------
+models = {
+    "1": {"name": "gpt-4o-mini", "precio por 1M de tokens": 0.15},
+    "2": {"name": "gpt-4.1-mini", "precio por 1M de tokens": 0.40},
+    "3": {"name": "gpt-4.1", "precio por 1M de tokens": 2.00},
+    "4": {"name": "gpt-5-nano", "precio por 1M de tokens": 0.05},
+    "5": {"name": "gpt-5-mini", "precio por 1M de tokens": 0.25},
+}
+
+# -----------------------------
+# FUNCIONES
+# -----------------------------
+
+def estimate_tokens(text):
+    """
+    Estima el número de tokens de un texto de entrada.
+
+    Utiliza una aproximación simple donde 1 token ≈ 4 caracteres.
+
+    Args:
+        text (str): Texto de entrada proporcionado por el usuario.
+
+    Returns:
+        int: Número estimado de tokens del texto.
+    """
+    # Aproximación común: 1 token ≈ 4 caracteres
+    return math.ceil(len(text) / 4)
+
+
+def estimate_cost(tokens, price_per_1m):
+    """
+    Calcula el coste estimado de entrada basado en el número de tokens.
+
+    El cálculo se basa en el precio por millón de tokens definido para cada modelo.
+
+    Args:
+        tokens (int): Número estimado de tokens de entrada.
+        price_per_1M (float): Precio del modelo por 1 millón de tokens.
+
+    Returns:
+        float: Coste estimado en USD de la entrada.
+    """
+    return (tokens / 1000000) * price_per_1m
+
+
+def select_model(tokens):
+    """
+    Permite al usuario seleccionar un modelo de lenguaje disponible.
+
+    Muestra una lista de modelos junto con el coste estimado de la consulta
+    basado en los tokens de entrada.
+
+    Args:
+        tokens (int): Número estimado de tokens de la consulta.
+
+    Returns:
+        dict: Diccionario con la configuración del modelo seleccionado.
+    """
+    print("\nSelecciona un modelo:\n")
+    for key, model in models.items():
+         cost = estimate_cost(tokens, model["precio por 1M de tokens"])
+         print(f"{key}. {model['name']} → Coste estimado: ${cost:.6f}")
+
+    while True:
+        try:
+            choice = input("\nOpción: ")
+
+            if choice in models:
+                model = models[choice]
+                print(f"Modelo elegido: {model['name']}")
+                return model
+            else:
+                print("Modelo no válido")
+
+        except ValueError:
+            print("Debes introducir un número válido.")
+                
+
+def select_max_tokens():
+    """
+    Solicita al usuario el número máximo de tokens de salida.
+
+    Si el usuario no introduce ningún valor, se considera ilimitado (None).
+
+    Returns:
+        int | None: Número máximo de tokens de salida o None si no se especifica límite.
+    """
+    while True:
+        value = input("\nMáximo de tokens de salida (Enter = ilimitado): ")
+
+        if value.strip() == "":
+            print("Máxima cantidad de tokens de salida: Ilimitado")
+            return None
+
+        try:
+            value_int = int(value)
+
+            if value_int > 0:
+                print(f"Máxima cantidad de tokens de salida: {value_int}")
+                return value_int
+            else:
+                print("El valor debe ser mayor que 0.")
+
+        except ValueError:
+            print("El valor no es un número entero.")
+
+
+def select_temperature():
+    """
+    Permite al usuario seleccionar el nivel de creatividad del modelo.
+
+    Se ofrecen varias opciones que corresponden a valores de temperatura.
+
+    Returns:
+        float: Valor de temperatura seleccionado para la generación de texto.
+    """
+    print("\nNivel de creatividad:")
+    print("1. Muy preciso (0.2)")
+    print("2. Equilibrado (0.5)")
+    print("3. Creativo (0.8)")
+    print("4. Muy creativo (1.0)")
+
+    mapping = {
+        1: 0.2,
+        2: 0.5,
+        3: 0.8,
+        4: 1.0
+    }
+
+    while True:
+        choice = input("Opción: ")
+
+        try:
+            value_int = int(choice)
+
+            if value_int in mapping:
+                temperature = mapping[value_int]
+                print(f"Temperatura utilizada: {temperature}")
+                return temperature
+            else:
+                print("Opción no válida. Elige entre 1 y 4.")
+
+        except ValueError:
+            print("El valor no es un número entero.")
+    
+    
+
+
+def query_model(prompt, model_name, max_tokens, temperature):
+    """
+    Envía una consulta al modelo de lenguaje y devuelve la respuesta generada.
+
+    Args:
+        prompt (str): Texto de entrada del usuario.
+        model_name (str): Nombre del modelo a utilizar.
+        max_tokens (int | None): Límite máximo de tokens de salida.
+        temperature (float): Nivel de creatividad del modelo.
+
+    Returns:
+        str: Respuesta generada por el modelo o mensaje de error en caso de fallo.
+    """
+    response = client.responses.create(
+        model=model_name,
+        input=prompt,
+        max_output_tokens=max_tokens,
+        temperature=temperature
+    )
+    
+    start = time.perf_counter()
+    end = time.perf_counter()
+
+    latency = end - start
+
+    print(f"Cantidad de tokens de input: {response.usage.input_tokens}")
+    print(f"Cantidad de tokens de output: {response.usage.output_tokens}")
+    print(f"tiempo de procesamiento (latencia) en segundos: {latency}")
+    return response.output[0].content[0].text
+
+
+
+# -----------------------------
+# FLUJO PRINCIPAL
+
+def main():
+    """
+    Función principal que ejecuta el flujo interactivo del programa.
+
+    Permite al usuario:
+    - Introducir una pregunta
+    - Ver el número estimado de tokens
+    - Seleccionar un modelo de lenguaje
+    - Definir el límite de tokens de salida
+    - Elegir el nivel de creatividad
+    - Obtener una respuesta generada por el modelo
+
+    El programa se ejecuta en bucle hasta que el usuario decide salir.
+    """
+    
+    while True:
+        # 1. Input usuario
+        print("Oprime 1 para preguntar")
+        print("Oprime 2 para salir")
+        menu = input()
+
+        if menu == "1":
+            
+            prompt = input("Introduce tu pregunta:\n")
+            
+            if not prompt.strip():
+                print("La pregunta no puede estar vacía.\n")
+                continue
+            # 2. Tokens estimados
+            tokens = estimate_tokens(prompt)
+            print(f"\nTokens estimados: {tokens}")
+
+            # 3. Selección modelo
+            model = select_model(tokens)
+                
+            # 4. Max tokens
+            max_tokens = select_max_tokens()
+
+            # 5. Creatividad
+            temperature = select_temperature()
+
+            # 6. Llamada al modelo
+            print("\nConsultando modelo...\n")
+
+            answer = query_model(
+                prompt,
+                model["name"],
+                max_tokens,
+                temperature
+            )
+
+            print("Respuesta:\n")
+            print(answer)
+
+        elif menu == "2":
+            print("¡Hasta pronto!")
+            break
+        
+        else:
+            print("Opción no válida")
+        
+
+
+if __name__ == "__main__":
+    main()
